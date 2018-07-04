7c5fececb14c7e60c59f8836b0f7fa87d9456828
richard <ysy123@gmail.com>
2018-07-04 13:28:28 +0800

    Updated toString method

--- src/org/paumard/model/User.java
+++ src/org/paumard/model/User.java
@@ -11,11 +11,6 @@ public class User {
     public void setName(String name) {
         this.name = name;
     }
-
-    @Override
-    public String toString() {
-        return "User [name=" + name + "]";
-    }
     
 
     public int getAge() {
@@ -25,5 +20,12 @@ public class User {
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "User [name=" + name + ", age=" + age + "]";
+    }
+    
+    
 
 }
